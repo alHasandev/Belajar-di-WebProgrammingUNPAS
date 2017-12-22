d34d78ebc069480c6bdda76f206578e79b20f60b
Muhammad Albie Al Hasan <alhasan99dev@gmail.com>
2017-12-22 12:08:11 +0800

    Add files via upload
    pertemuan ke 6

--- phpdasar/latihan1.php
+++ phpdasar/latihan1.php
@@ -1,33 +1,76 @@
 <?php 
-// array
-// sebuah variabel yang dapat memiliki banyak nilai
-// elemen pada array di PHP boleh memiliki tipe data yang berbeda
+// // Associative Array
+// // cara lama
+// $hari = array("senin", "selasa", "rabu");
 
-// Membuat Array
-// cara lama
-$hari = array("Senin","Selasa","Rabu","Kamis","Jum'at","Sabtu","Minggu");
+// // cara baru
+// $bulan = ["januari", "pebruari", "maret"];
 
-// Cara Baru
-// $hari = ["Senin","Selasa","Rabu","kamis","Jum'at","Sabtu","Minggu"];
-$bulan = ["Januari","Pebruari","Maret","April"];
-$arr1 = [123, "tulisan", false];
-
-
-// Menampilkan Array
-
-// var_dump($hari);
+// // Menampilkan Array
+// var_dump($bulan);
 // echo "<br>";
+
 // print_r($bulan);
 // echo "<br>";
 
-// Menampilkan 1 elemen array
+// echo $hari[2];
+// echo "<br>";
 
-// echo $arr1[0];
+// for( $i = 0; $i < 3; $i++ ) {
+// 	echo $bulan[$i];
+// 	echo "<br>";
+// }
 
-// Menambahkan elemen baru pada array
-print_r($bulan);
-$bulan[] = "Mei";
-$bulan[] = "Juni";
-echo "<br>";
-print_r($bulan);
- ?>
+// foreach( $bulan as $b ) {
+// 	echo $b;
+// 	echo "<br>";
+// }
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<style>
+		.kotak {
+			width: 50px;
+			height: 50px;
+			background-color: lime;
+			box-shadow: 0px 4px 10px -2px rgba(0,0,0,0.5); 
+			text-align: center;
+			float: left;
+			margin: 3px;
+			line-height: 50px;
+			transition: .5s;
+		}
+		.kotak:hover {
+			transform: rotate(360deg);
+			border-radius: 50%;
+		}
+
+		.clear {
+			clear: both;
+		}
+	</style>
+	<title>latihan Array</title>
+</head>
+<body>
+	<?php 
+		$angka = [
+			[1,2,3],
+			[4,5,6],
+			[7,8,9]
+		];	
+	 ?>
+
+	<?php foreach( $angka as $a ) : ?>
+		<?php foreach( $a as $b ) : ?>
+			<div class="kotak"><?= $b; ?></div>
+		<?php endforeach; ?>
+
+		<div class="clear"></div>
+	<?php endforeach; ?>
+
+</body>
+</html>
--- phpdasar/latihan2.php
+++ phpdasar/latihan2.php
@@ -1,39 +1,51 @@
 <?php 
-// Pengulangan pada array
-// for / foreach
-$angka = [3,2,15,20,11,77,89,99];
+	// $mahasiswa = [
+	// 	["Mohamad Albie", "0427", "Teknik Informatika", "mohamad9albie@gmail.com"],
+	// 	["Muhammad Amien Hidayat", "0165", "Teknik Informatika", "amienb7@gmail.com"],
+	// 	["Muhammad Azhari", "0111", "Teknik Informatika", "ariboy@gmail.com"]
+	// ];
 
- ?>
+// Array Associative
+// Deifinisinya sama seperti array numerik, kecuali
+// keynya adalah string yang kita buat sendiri
+$mahasiswa = [
+	[
+		"nama" => "Mohamad Albie", 
+		"npm" => "0427",
+		"jurusan" => "Teknik Informatika",
+		"email" => "mohamad9albie@gmail.com",
+		"gambar" => "profil1.jpg"
+	],
+	[
+		"npm" => "0165",
+		"nama" => "Muhammad Amien Hidayat", 
+		"jurusan" => "Teknik Informatika",
+		"email" => "amienb7@gmail.com",
+		"gambar" => "profil2.png"
+	]
+
+];
+
+?>
 
  <!DOCTYPE html>
  <html lang="en">
  <head>
  	<meta charset="UTF-8">
- 	<title>Latihan 2</title>
- 	<style>
- 		div.kotak {
- 			width: 50px;
- 			height: 50px;
- 			background-color: salmon;
- 			text-align: center;
- 			line-height: 50px;
- 			margin: 3px;
- 			float: left;
- 		}
- 		div.clear {
- 			clear: both;
- 		}
- 	</style>
+ 	<title>Latihan Array 2</title>
  </head>
  <body>
- 	<?php for( $i = 0; $i < count($angka); $i++ ) { ?>
-		<div class="kotak"><?php echo $angka[$i]; ?></div>
-	<?php }  ?>
-
-	<div class="clear"></div>
-
-	<?php foreach( $angka as $a ) : ?>
-		<div class="kotak"><?php echo $a; ?></div>
-	<?php endforeach; ?>
+ 	<h1>Daftar Mahasiswa</h1>
+ 	<?php foreach( $mahasiswa as $mhs ) : ?>
+ 	<ul>
+ 		<li>
+ 			<img src="img/<?php echo $mhs["gambar"]; ?>" alt="">
+ 		</li>
+ 		<li>Nama : <?= $mhs["nama"]; ?></li>
+ 		<li>NPM : <?= $mhs["npm"]; ?></li>
+ 		<li>Jurusan : <?= $mhs["jurusan"]; ?></li>
+ 		<li>Email : <?= $mhs["email"]; ?></li>
+ 	</ul>
+ 	<?php endforeach; ?>
  </body>
  </html>
